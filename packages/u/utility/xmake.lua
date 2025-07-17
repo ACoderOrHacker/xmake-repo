39afd9d98e24db3ebda775ab0467921f6f5d2b76
ACoder <sgy2788@163.com>
2025-07-17 12:58:40 +0800

    feat(utility): add utility module

---
+++ packages/u/utility/xmake.lua
@@ -0,0 +1,15 @@
+package("utility")
+    set_kind("library", {headeronly = true})
+    set_description("Utility Tools Library")
+
+    add_urls("https://github.com/ACoderOrHacker/ox.utility.git")
+    add_versions("0.1.0", "9691db4fdd08896a95fcbdefde678a0f1a5d252e")
+
+    on_install(function (package)
+        import("package.tools.xmake").install(package, configs)
+    end)
+
+    on_test(function (package)
+        assert(package:has_cxxfuncs("ox::debugbreak", {includes = "ox/utility/debugbreak.hpp"})
+        assert(package:has_cxxfuncs("ox::bytes_of<int>", {includes = "ox/utility/size.hpp"}))
+    end)
