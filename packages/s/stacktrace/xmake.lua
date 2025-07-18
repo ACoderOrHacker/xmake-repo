997a8d0a6a72949123fb90e32a04bcb8ae8b02b1
ACoder <sgy2788@163.com>
2025-07-18 21:36:04 +0800

    fix(stacktrace, termcolor): fix include patterns

--- packages/s/stacktrace/xmake.lua
+++ packages/s/stacktrace/xmake.lua
@@ -3,7 +3,7 @@ package("stacktrace")
     set_description("Stacktrace information getting library")
 
     add_urls("https://github.com/ACoderOrHacker/ox.stacktrace.git")
-    add_versions("0.1.0", "c7b78ac9c57b597d2f68d68fe4a66e472c77d062")
+    add_versions("0.1.0", "ac05ce3db74c4c3a05ee783ef24e25698454d726")
 
     on_install(function (package)
         import("package.tools.xmake").install(package, configs)
--- packages/t/termcolor/xmake.lua
+++ packages/t/termcolor/xmake.lua
@@ -3,7 +3,7 @@ package("termcolor")
     set_description("A library that supports setting color flags to print colorful character")
 
     add_urls("https://github.com/ACoderOrHacker/ox.termcolor.git")
-    add_versions("0.1.0", "7b1cfa457ad02f3d1d1226a49c7c227c937970b1")
+    add_versions("0.1.0", "55a8bb4f763c5c38aa6e4ea08f517b933a99c7fe")
 
     on_install(function (package)
         import("package.tools.xmake").install(package, configs)
