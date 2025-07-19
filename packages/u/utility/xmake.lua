f87646912b414254696e645101613c7c829083f5
ACoder <sgy2788@163.com>
2025-07-19 09:08:14 +0800

    feat(utility): add utility 0.1.1

--- packages/u/utility/xmake.lua
+++ packages/u/utility/xmake.lua
@@ -4,6 +4,7 @@ package("utility")
 
     add_urls("https://github.com/ACoderOrHacker/ox.utility.git")
     add_versions("0.1.0", "2854526875f6015840fc51775ab0b3a88c3e986d")
+    add_versions("0.1.1", "f50ec81a2fe501c553e766c598c214b7d316a7af")
 
     on_install(function (package)
         import("package.tools.xmake").install(package, configs)
